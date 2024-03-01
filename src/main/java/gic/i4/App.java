fd03563f1de2de4523398ef50cb3abffc7c4b8e1
SenghongVat <vatsenghong@gmail.com>
2024-03-01 10:12:09 +0700

    first commit

---
+++ src/main/java/gic/i4/App.java
@@ -0,0 +1,13 @@
+package gic.i4;
+
+/**
+ * I4 TP 1 Autobuild trigger
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I4 TP 1 Autobuild trigger" );
+    }
+}
